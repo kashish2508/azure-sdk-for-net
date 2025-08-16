38fd660f16e2c3703f0213d871034049ee7eae03
guptakashish <guptakashish@microsoft.com>
2025-08-16 21:20:00 +0530

    local Test Setup

---
+++ sdk/loadtestservice/Azure.Developer.Playwright.NUnit/tempSample/PlaywrightTests/PlawrightServiceSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using Azure.Developer.Playwright;
+using Azure.Developer.Playwright.NUnit;
+using Azure.Identity;
+using NUnit.Framework;
+
+namespace PlaywrightTests;     // Remember to change this as per your project namespace
+
+[SetUpFixture]
+public class PlaywrightServiceNUnitSetup : PlaywrightServiceBrowserNUnit
+{
+    public PlaywrightServiceNUnitSetup() : base(
+        credential: new DefaultAzureCredential(),
+        options: new PlaywrightServiceBrowserClientOptions
+        {
+
+            UseCloudHostedBrowsers = true,
+            OS = OSPlatform.Linux,
+            ExposeNetwork = "<loopback>",
+            RunId = Guid.NewGuid().ToString(),
+            //   RunName = "Github action test",
+            ServiceAuth = ServiceAuthType.EntraId
+        }
+    )
+    { }
+}
